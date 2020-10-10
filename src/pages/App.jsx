f7183b634fde8f6a7c618ea87c699cceb021703e
unknown <komarudin2110@gmail.com>
2020-10-10 10:30:38 +0700

    Add Repo Component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,28 +1,64 @@
 import React, { useEffect, useState } from 'react'
-import axios from '../config/axios'
-import './App.scss'
+
+// Component
 import { Header } from '../components'
 
+// Style
+import './App.scss'
+
+// Packages
+import axios from '../config/axios'
+import moment from 'moment'
 
 const App = () => {
   const clientId = process.env.CLIENT_ID
   const [userAcc, setUserAcc] = useState('')
+  const [repos, setRepos] = useState('')
   const [search, setSearch] = useState('komarudinn')
+  const [totalCard, setTotalCard] = useState(4)
+  const [load, setLoad] = useState(false)
 
   useEffect(() => {
     searchAcc()
-  }, [clientId])
+  }, [])
 
-  const searchAcc = () => {
-    console.log(search);
-    axios.get(`/${search}?client_id=${clientId}`).then(res => {
-      console.log(res.data);
-      setUserAcc(res.data)
-    }).catch(err => {
-      console.log(err);
-    })
+  const searchAcc = async () => {
+    try {
+      setLoad(true)
+      let getUser = await axios.get(`/${search}?client_id=${clientId}`)
+      let getRepos = await axios.get(`/${getUser.data.login}/repos`)
+      setUserAcc(getUser.data)
+      setRepos(getRepos.data)
+      setLoad(false)
+    } catch (error) {
+      console.log(error.response.status);
+    }
   }
 
+
+  const rendRepos = (data) => {
+    if (data) {
+      return data.slice(0, totalCard).map((item, index) => {
+        return (
+          <div onClick={() => window.open(item.clone_url)} key={index} className="card  ">
+            <h4>{item.name}</h4>
+            <div className="card__content">
+              <small className="lang">{item.language ? item.language : 'Unknown'}</small>
+              <small>{moment(item.created_at).format('LL')}</small>
+            </div>
+          </div>
+        )
+      })
+    }
+  }
+
+  const changeCard = () => {
+    if (totalCard >= repos.length) {
+      setTotalCard(4)
+    } else {
+      setTotalCard(totalCard + 4)
+    }
+  }
 
   return (
     <div>
@@ -46,8 +82,22 @@ const App = () => {
           <small className="info__icon"><i className="fa fa-user-o" aria-hidden="true"></i> {userAcc.followers} Followers</small>
         </div>
       </div>
+
+      {/* Main Repository */}
+      <div className="repos">
+        <h2>Repositories</h2>
+        <div className="">
+          {rendRepos(repos)}
+        </div>
+        <div className="repos__button">
+          <button onClick={() => changeCard()}>
+            {totalCard >= repos.length ? 'Less More' : 'See More'}
+          </button>
+        </div>
+      </div>
     </div>
   )
+
 }
 
 export default App
