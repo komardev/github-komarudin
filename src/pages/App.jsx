7520ba0e231f66e016b2ad6b42bfd788850c7f9d
unknown <komarudin2110@gmail.com>
2020-10-10 09:21:04 +0700

    FIx profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -7,15 +7,21 @@ import { Header } from '../components'
 const App = () => {
   const clientId = process.env.CLIENT_ID
   const [userAcc, setUserAcc] = useState('')
+  const [search, setSearch] = useState('komarudinn')
 
   useEffect(() => {
-    axios.get(`/komarudinn?client_id=${clientId}`).then(res => {
+    searchAcc()
+  }, [clientId])
+
+  const searchAcc = () => {
+    console.log(search);
+    axios.get(`/${search}?client_id=${clientId}`).then(res => {
       console.log(res.data);
       setUserAcc(res.data)
     }).catch(err => {
       console.log(err);
     })
-  }, [clientId])
+  }
 
 
   return (
@@ -24,17 +30,20 @@ const App = () => {
       {/* Searchbar */}
       <div className="search">
         <div className="search__input">
-          <input type="text" placeholder="Enter a Github username..." />
-          <i className="fa fa-search"></i>
+          <input onChange={(e) => { setSearch(e.target.value) }} value={search} type="text" placeholder="Enter a Github username..." />
+          <i onClick={() => searchAcc()} className="fa fa-search"></i>
         </div>
       </div>
 
       {/* Main Content */}
       <div onClick={() => window.open(userAcc.html_url)} className="main">
         <img className="main__image" src={userAcc.avatar_url} alt="" />
-        <div>
-          <h3 className="main__name">{userAcc.name}</h3>
-          <h3 className="main__username">@{userAcc.login}</h3>
+        <div className="info">
+          <h3 className="info__name">{userAcc.name}</h3>
+          <h3 className="info__username">@{userAcc.login}</h3>
+          {userAcc.location && (<h5 className="info__location"><i className="fa fa-map-marker" aria-hidden="true"></i> {userAcc.location}</h5>)}
+          <small className="info__icon"><i className="fa fa-folder-o" aria-hidden="true"></i> {userAcc.public_repos} Repos</small>
+          <small className="info__icon"><i className="fa fa-user-o" aria-hidden="true"></i> {userAcc.followers} Followers</small>
         </div>
       </div>
     </div>
